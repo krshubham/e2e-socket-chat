1c866dcbbe5b78a235e38e7da189fdf84a8cbf16
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2016-10-23 00:35:54 +0530

    Client js code init

---
+++ public/js/main.js
@@ -0,0 +1,36 @@
+function chat(e) {
+    e.preventDefault();
+    return false;
+}
+
+
+var socket = io("http://localhost:3000");
+
+socket.on("disconnect", function() {
+	setTitle("Disconnected");
+});
+
+socket.on("connect", function() {
+	alert('Hello');
+});
+
+socket.on("message", function(message) {
+	printMessage(message);
+});
+
+document.forms[0].onsubmit = function () {
+    var input = document.getElementById("message");
+    printMessage(input.value);
+    socket.emit("chat", input.value);
+    input.value = '';
+};
+
+function setTitle(title) {
+    document.querySelector("h1").innerHTML = title;
+}
+
+function printMessage(message) {
+    var p = document.createElement("p");
+    p.innerText = message;
+    document.querySelector("div.messages").appendChild(p);
+}

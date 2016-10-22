62550920783ce57e40e21f7011e798884324c49e
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2016-10-22 14:55:57 +0530

    index view up

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var mustacheExpress = require('mustache-express');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname,'public')));
+app.engine('html',mustacheExpress());
+app.set('view engine','html');
+app.set('views',path.join(__dirname,'views'));
+
 
 app.get('/',function(req,res){
-    res.send('hello');
+    console.log('request for index page');
+    res.render('index',{
+        title: 'Welcome'
+    });
 });
 
 console.log('server listening at port 3000');

e99798f40090383d98ef90b9e0998a64a3a3e340
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2017-01-08 19:27:39 +0530

    Added auth complete

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ var path = require('path');
 var mustacheExpress = require('mustache-express');
 var io = require('socket.io')(server);
 var moment = require('moment');
+var auth = require('./routes/auth');
+var morgan = require('morgan');
+var db = require('./db');
 
+
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,32 +19,41 @@ app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
 
-io.on('connection', function (socket) {
-    console.log(socket.handshake);
-
-    socket.on('chat', function (data) {
-        var time = new Date().getTime();
-        var content = data;
-        var reply = {
-            time: moment().from(time),
-            content: content
-        };
-        socket.emit('message',reply);        
-        socket.broadcast.emit('message',reply);
-    });
-    socket.on('typing',function(){
-        socket.emit('typemsg');
-    });
+//db connection
+const url = 'mongodb://localhost:27017/fun';
+db.connect(url, function (err) {
+    if(err){
+        console.log(err);
+    }
+    console.log('db conn active');
 });
+
+// io.on('connection', function (socket) {
+//     console.log(socket.handshake);
+
+//     socket.on('chat', function (data) {
+//         var time = new Date().getTime();
+//         var content = data;
+//         var reply = {
+//             time: moment().from(time),
+//             content: content
+//         };
+//         socket.emit('message',reply);        
+//         socket.broadcast.emit('message',reply);
+//     });
+//     socket.on('typing',function(){
+//         socket.emit('typemsg');
+//     });
+// });
 
 
 app.get('/', function (req, res) {
-    console.log('request for index page');
     res.render('index', {
         title: 'Welcome'
     });
 });
 
+app.post('/auth/login',auth.login);
 
 console.log('server listening at port 3000');
-server.listen(3000);
+server.listen(80);

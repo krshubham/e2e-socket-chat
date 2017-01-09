1e2ad78b99dfda5f51e78de5dfe3a149b0c97ed7
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2017-01-09 18:10:46 +0530

    Enhancing chat UI

--- public/js/main.js
+++ public/js/main.js
@@ -1,66 +1,17 @@
-function chat(e) {
-    e.preventDefault();
-    return false;
+var socket = io.connect('http://localhost/app');
+
+function setTitle(text){
+    var title = document.getElementsByTagName('title')[0];
+    title.innerHTML = text;
 }
 
-
-var socket = io();
-var title = document.getElementsByTagName('title')[0];
-
-
 socket.on("disconnect", function () {
-    //alert('disconnected');
-    setTitle('disconnected | Chat');    
-    
+    setTitle('disconnected | Chat');
 });
 
 socket.on("connect", function () {
-    //alert('Hello');
-    setTitle('connected | Chat');    
+    setTitle('connected | Chat');
 });
 
-socket.on("message", function (message) {
-    printMessage(message);
-});
 
-socket.on("typemsg",function(data){
-    printType(data);
-})
-function printMessage(message) {
-   var html = ' <div class="message">'+
-        '<div class="card-panel black">'+
-            '<div class="chip">'+
-                message.time+
-            '</div>'+
-            '<p class="chat-p">'+message.content+'</p>'+
-       '</div>'+
-    '</div>';
-    document.querySelector("div.messages").innerHTML += html;
-}
-
-function printType(data){
-    document.getElementById("type").style.display = 'block';
-}
-setInterval(function(){
-    document.getElementById('type').style.display = 'none';
-},5000);
-function chat(e){
-    e.preventDefault();
-    var message = null || document.getElementById('chatmsg').value;
-    console.log(message);
-    if(message){
-        socket.emit('chat',message);
-    }
-    else{
-        alert('Fill something in');
-    }
-    return false;
-}
-function typing(){
-    socket.emit('typing');
-}
-
-function setTitle(text){
-    title.innerHTML = text;
-}
 

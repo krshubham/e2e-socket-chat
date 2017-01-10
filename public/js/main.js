28a36962fc742e2797f8a7bae2bbe8cd0d72abc2
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2017-01-10 10:55:29 +0530

    chat and broadcast working

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,26 @@ function setTitle(text){
     title.innerHTML = text;
 }
 
+function getMessage(e) {
+    e.preventDefault();
+    console.log({data: $('#chatmsg').val(),
+        token: location.pathname.split('/')[2]
+    });
+    socket.emit('publicmsg', {
+        data: $('#chatmsg').val(),
+        token: location.pathname.split('/')[2]
+    });
+}
+
+socket.on('pubmsg', function (data) {
+    var html = `<div class="card white-text black">
+            <h3>Message: ${data.data}</h3><br>
+            <strong>By: ${data.username}</strong>            
+    </div>`;
+    $('div.messages').append(html);
+
+});
+
 socket.on("disconnect", function () {
     setTitle('disconnected | Chat');
 });

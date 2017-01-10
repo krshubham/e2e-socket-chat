24774c3d8a290890ba045449b1833cffb91c74e9
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2017-01-10 21:36:11 +0530

    Message history using db added

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 var socket = io.connect('http://35.154.38.81:9876/app');
+// var socket = io.connect('http://localhost:9876/app');
 
 function setTitle(text){
     var title = document.getElementsByTagName('title')[0];
@@ -17,7 +18,7 @@ function getMessage(e) {
     $('#chatmsg').val('');
 }
 
-socket.on('pubmsg', function (data) {
+function printMessage(data){
     var html = `<div class="card white-text black">
             <h3>Message: ${data.data}</h3><br>
             <strong>By: ${data.username}</strong>            
@@ -25,7 +26,9 @@ socket.on('pubmsg', function (data) {
     $('div.messages').append(html);
     var messages = document.getElementsByClassName('messages')[0];
     messages.scrollTop = messages.scrollHeight;
-
+}
+socket.on('pubmsg', function (data) {
+    printMessage(data);
 });
 
 socket.on("disconnect", function () {
@@ -34,6 +37,18 @@ socket.on("disconnect", function () {
 
 socket.on("connect", function () {
     setTitle('connected | Chat');
+    socket.emit('getMessages',{
+        token: location.pathname.split('/')[2]
+    });
+});
+
+socket.on('printLastSession', function(result){
+    var messages = result.data;
+    if(messages.length){
+        messages.forEach(function(message){
+            printMessage(message);
+        });
+    }
 });
 
 

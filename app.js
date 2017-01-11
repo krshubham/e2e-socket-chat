e2e84278179bbb3bc7768812e4fc6a175ff74711
Kumar Shubham <kumar.shubham@wingify.com>
2017-01-12 00:13:37 +0530

    Added cron job for cleaning messages

--- app.js
+++ app.js
@@ -11,7 +11,26 @@ var morgan = require('morgan');
 var db = require('./db');
 var chat = require('./routes/chat');
 var mainApp = require('./routes/main-app');
+var Agenda = require('agenda');
+var connString = 'mongodb://localhost:27017/agenda';
 
+var agenda = new Agenda({db: {address: connString}});
+
+//Cron job to delete chats every day
+agenda.define('delete old messages', function(job, done){
+    var messages = db.get().collection('messages');
+    messages.remove({}).then(function(){
+        done();
+    })
+    .catch(function(err){
+        console.log(err);
+    });
+});
+
+agenda.on('ready', function(){
+    agenda.every('24 hours', 'delete old messages');
+    agenda.start();
+});
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));

d82141554c26347903cb1f8bb6451d71bdc8b83b
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2017-01-13 22:27:14 +0530

    Online users added

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,14 @@
-// var socket = io.connect('http://35.154.38.81:9876/app');
-var socket = io.connect('http://localhost:9876/app');
+//IP Addresses to use
+/*************************************************************************************
+ * For server usage: http://35.154.38.81/app                                         *
+ *                                                                                   *
+ * For localhost usage: http://localhost:9876/app                                    *
+ *                                                                                   *
+ * For usage in local network: http://192.168.1.100:9876/app                         *
+ * This one keeps changing if you change your network or reconnect some other time   *
+ **************************************************************************************/
+
+var socket = io.connect('http://35.154.38.81:9876/app');
 
 function setTitle(text){
     var title = document.getElementsByTagName('title')[0];
@@ -25,37 +34,65 @@ function getMessage(e) {
 function printMessage(data){
     var html = `<div class="card white-text black">
             <h3>Message: ${data.data}</h3><br>
-            <strong>By: ${data.username}</strong>            
+            <strong>By: ${data.username}</strong>          
     </div>`;
     $('div.messages').append(html);
     var messages = document.getElementsByClassName('messages')[0];
     messages.scrollTop = messages.scrollHeight;
 }
+
+function insertOnlineUser(person){
+    var html = `
+                <div class="card black" id="${person._id}">
+                <strong>Username: <span class="white-text">${person.username}</span></strong>
+    `;
+    var onlinediv = document.getElementById('online');
+    $('div.online').append(html);
+    onlinediv.scrollTop = onlinediv.scrollHeight;
+    Materialize.toast('User connected',2000);
+}
+
 socket.on('pubmsg', function (data) {
     printMessage(data);
 });
 
 socket.on("disconnect", function () {
-    setTitle('disconnected | Chat');
+    remove
 });
 
 socket.on("connect", function () {
     setTitle('connected | Chat');
-    socket.emit('getMessages',{
+    socket.emit('getData',{
         token: location.pathname.split('/')[2]
     });
 });
 
 socket.on('printLastSession', function(result){
     var messages = result.data;
+    var onlineUsers = result.users;
     if(messages.length){
         messages.forEach(function(message){
             printMessage(message);
+        });
+    }
+    if(onlineUsers.length){
+        console.log(onlineUsers);
+        onlineUsers.forEach(function (user) {
+            insertOnlineUser(user);
         });
     }
 });
 
 socket.on('jwterror', function (data) {
     Materialize.toast(data.message,3000);
+});
+
+socket.on('fatalerr', function (data) {
+    console.log(data);
+    Materialize.toast('Please login again',3000);
+});
+
+socket.on('user connected', function (data) {
+    insertOnlineUser(data.user);
 });
 
